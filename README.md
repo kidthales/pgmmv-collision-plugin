2abab6b8be5ddcda1d699084838f4d30b071ef29
kidthales <kidthales@agogpixel.com>
2026-03-24 01:12:44 -0700

    Link Conditions (#5)
    * Declare initial link condition configs; wire up and implement 'On Tile Wall or Slope' link condition.
    
    * Finish the initial link conditions.
    
    * Update README and plugin.

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](./LICENSE)
 
 Utilities for working with 2D collisions in [PGMMV](https://rpgmakerofficial.com/product/act/en/manual/01_01.html).
+
+| Link Condition            | Description                                                                                                                                                                                     |
+| ------------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| On Wall or Slope          | Test if object instance bottom wall collision is touching a tile wall or object instance is touching a slope from the top. Leaving the tile group inputs unset will use the default tile group. |
+| On Slope Facing Downslope | Test if object instance is touching a slope and facing downslope.                                                                                                                               |
+| On Slope Facing Upslope   | Test if object instance is touching a slope and facing upslope.                                                                                                                                 |
--- plugin.js
+++ plugin.js
@@ -12,18 +12,17 @@
    * @type {import("pgmmv-types/lib/agtk/plugins/plugin").AgtkPlugin}
    */
   var plugin = {
-      setLocale: function (locale) {},
-
+      setLocale: function () {},
       getInfo: function (category) {
         switch (category) {
           case 'name':
-            return 'PGMMV Plugin Template';
+            return 'PGMMV Collision Plugin';
           case 'description':
-            return 'PGMMV Plugin Template';
+            return 'Utilities for working with 2D collisions.';
           case 'author':
-            return 'Author Name';
+            return 'Tristan Bonsor <kidthales@agogpixel.com>';
           case 'help':
-            return 'Plugin Help';
+            return '';
           case 'parameter':
             return [];
           case 'internal':
@@ -31,63 +30,253 @@
           case 'actionCommand':
             return [];
           case 'linkCondition':
-            return [];
+            return [
+              onTileWallOrSlopeLinkCondition,
+              onSlopeFacingDownslopeLinkCondition,
+              onSlopeFacingUpslopeLinkCondition
+            ];
           default:
             break;
         }
       },
-
-      initialize: function (data) {
+      initialize: function () {
         if (isEditor()) {
           return;
         }
 
-        Agtk.log('Initialize ' + plugin.getInfo('name'));
-      },
+        if (!window.kt) {
+          window.kt = {};
+        }
 
+        window.kt.collision = {
+          onTileWallOrSlope: onTileWallOrSlope,
+          onSlopeFacingDownslope: onSlopeFacingDownslope,
+          onSlopeFacingUpslope: onSlopeFacingUpslope
+        };
+      },
       finalize: function () {},
-
-      setParamValue: function (paramValue) {
-        if (isEditor()) {
-          return;
-        }
-
-        for (var i = 0; i < paramValue.length; ++i) {
-          Agtk.log('Parameter ' + paramValue[i].id + ' = ' + paramValue[i].value);
-        }
-      },
-
-      setInternal: function (data) {},
-
+      setParamValue: function () {},
+      setInternal: function () {},
       call: function () {},
+      execLinkCondition: function (linkConditionIndex, parameter, objectId, instanceId) {
+        /** @type {import("pgmmv-types/lib/agtk/plugins/plugin").AgtkLinkCondition} */
+        var linkCondition = plugin.getInfo('linkCondition')[linkConditionIndex],
+          /** @type {Record<number,import("type-fest").JsonValue>} */
+          np = normalizeParameters(parameter, linkCondition.parameter);
 
-      update: function (delta) {},
+        switch (linkCondition.id) {
+          case onTileWallOrSlopeLinkCondition.id:
+            return onTileWallOrSlope(np[linkCondition.parameter[0].id], np[linkCondition.parameter[1].id], instanceId);
+          case onSlopeFacingDownslopeLinkCondition.id:
+            return onSlopeFacingDownslope(instanceId);
+          case onSlopeFacingUpslopeLinkCondition.id:
+            return onSlopeFacingUpslope(instanceId);
+          default:
+            break;
+        }
 
-      execActionCommand: function (
-        actionCommandIndex,
-        parameter,
-        objectId,
-        instanceId,
-        actionId,
-        commandId,
-        commonActionStatus,
-        sceneId
-      ) {},
+        return false;
+      }
+    },
+    /** @type {import("pgmmv-types/lib/agtk/plugins/plugin").AgtkLinkCondition} */
+    onTileWallOrSlopeLinkCondition = {
+      id: 0,
+      name: 'On Wall or Slope [PGMMV Collision Plugin]',
+      description:
+        'Test if object instance bottom wall collision is touching a tile wall or object instance is touching a slope from the top. Leaving the tile group inputs unset will use the default tile group.',
+      parameter: [
+        {
+          id: 100,
+          name: 'Tile Group Variable Source:',
+          type: 'SwitchVariableObjectId',
+          option: ['SelfObject', 'ParentObject'],
+          defaultValue: -1
+        },
+        {
+          id: 0,
+          name: 'Tile Group:',
+          type: 'VariableId',
+          referenceId: 100,
+          withNewButton: true,
+          defaultValue: -1
+        }
+      ]
+    },
+    /** @type {import("pgmmv-types/lib/agtk/plugins/plugin").AgtkLinkCondition} */
+    onSlopeFacingDownslopeLinkCondition = {
+      id: 1,
+      name: 'On Slope Facing Downslope [PGMMV Collision Plugin]',
+      description: 'Test if object instance is touching a slope and facing downslope.',
+      parameter: []
+    },
+    /** @type {import("pgmmv-types/lib/agtk/plugins/plugin").AgtkLinkCondition} */
+    onSlopeFacingUpslopeLinkCondition = {
+      id: 2,
+      name: 'On Slope Facing Upslope [PGMMV Collision Plugin]',
+      description: 'Test if object instance is touching a slope and facing upslope.',
+      parameter: []
+    },
+    /**
+     * @param variableObjectId {
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['ProjectCommon'] |
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['SelfObject'] |
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['ParentObject']
+     * }
+     * @param tileGroupVariableId {number}
+     * @param instanceId {number}
+     * @returns {boolean}
+     */
+    onTileWallOrSlope = function (variableObjectId, tileGroupVariableId, instanceId) {
+      var tileGroupDefault = Agtk.constants.tileGroup.TileGroupDefault,
+        source = resolveSwitchVariableObject(variableObjectId, instanceId),
+        /**
+         * @type {
+         *   import("pgmmv-types/lib/agtk/variables/variable").AgtkVariable |
+         *   import("pgmmv-types/lib/agtk/object-instances/object-instance/variables/variable").AgtkVariable
+         * }
+         */
+        tileGroupVariable,
+        /** @type {number} */
+        tileGroup,
+        objectInstance = Agtk.objectInstances.get(instanceId);
 
-      execLinkCondition: function (
-        linkConditionIndex,
-        parameter,
-        objectId,
-        instanceId,
-        actionLinkId,
-        commonActionStatus
-      ) {}
+      if (source === Agtk.constants.actionCommands.UnsetObject) {
+        tileGroup = tileGroupDefault;
+      } else if (tileGroupVariableId < 1) {
+        tileGroup = tileGroupDefault;
+      }
+
+      if (tileGroup === undefined) {
+        tileGroupVariable = (
+          source === Agtk.constants.switchVariableObjects.ProjectCommon ? Agtk : source
+        ).variables.get(tileGroupVariableId);
+        tileGroup = !tileGroupVariable ? tileGroupDefault : tileGroupVariable.getValue();
+      }
+
+      return (
+        objectInstance.isWallTouched({
+          wallBit: Agtk.constants.tile.WallBottom,
+          useTileGroup: true,
+          tileGroup: tileGroup
+        }) ||
+        objectInstance.isSlopeTouched({
+          directionType: Agtk.constants.linkCondition.slopeTouched.DirectionUpper,
+          downwardType: Agtk.constants.linkCondition.slopeTouched.DownwardNone
+        })
+      );
+    },
+    /**
+     * @param instanceId {number}
+     * @returns {boolean}
+     */
+    onSlopeFacingDownslope = function (instanceId) {
+      var objectInstance = Agtk.objectInstances.get(instanceId),
+        direction = objectInstance.variables.get(Agtk.constants.objects.variables.DisplayDirectionId).getValue();
+
+      return (
+        (direction === 270 &&
+          objectInstance.isSlopeTouched({
+            directionType: Agtk.constants.linkCondition.slopeTouched.DirectionUpper,
+            downwardType: Agtk.constants.linkCondition.slopeTouched.DownwardLeft
+          })) ||
+        (direction === 90 &&
+          objectInstance.isSlopeTouched({
+            directionType: Agtk.constants.linkCondition.slopeTouched.DirectionUpper,
+            downwardType: Agtk.constants.linkCondition.slopeTouched.DownwardRight
+          }))
+      );
+    },
+    /**
+     * @param instanceId {number}
+     * @returns {boolean}
+     */
+    onSlopeFacingUpslope = function (instanceId) {
+      var objectInstance = Agtk.objectInstances.get(instanceId),
+        direction = objectInstance.variables.get(Agtk.constants.objects.variables.DisplayDirectionId).getValue();
+
+      return (
+        (direction === 270 &&
+          objectInstance.isSlopeTouched({
+            directionType: Agtk.constants.linkCondition.slopeTouched.DirectionUpper,
+            downwardType: Agtk.constants.linkCondition.slopeTouched.DownwardRight
+          })) ||
+        (direction === 90 &&
+          objectInstance.isSlopeTouched({
+            directionType: Agtk.constants.linkCondition.slopeTouched.DirectionUpper,
+            downwardType: Agtk.constants.linkCondition.slopeTouched.DownwardLeft
+          }))
+      );
     },
     /**
      * @returns {boolean}
      */
     isEditor = function () {
       return !Agtk || typeof Agtk.log !== 'function';
+    },
+    /**
+     * @param paramValue {import("pgmmv-types/lib/agtk/plugins/plugin").AgtkParameterValue[]} Parameter values to normalize.
+     * @param defaults {import("pgmmv-types/lib/agtk/plugins/plugin/parameter").AgtkParameter[]} Default parameter values available.
+     * @returns {Record<number, import("type-fest").JsonValue>}
+     */
+    normalizeParameters = function (paramValue, defaults) {
+      /** @type {Record<number,import("type-fest").JsonValue>} */
+      var normalized = {},
+        /** @type {number} */
+        len = defaults.length,
+        /** @type {number} */
+        i = 0,
+        /** @type {import("pgmmv-types/lib/agtk/plugins/plugin/parameter").AgtkParameter|import("pgmmv-types/lib/agtk/plugins/plugin").AgtkParameterValue} */
+        p;
+
+      for (; i < len; ++i) {
+        p = defaults[i];
+        normalized[p.id] = p.type === 'Json' ? JSON.stringify(p.defaultValue) : p.defaultValue;
+      }
+
+      len = paramValue.length;
+
+      for (i = 0; i < len; ++i) {
+        p = paramValue[i];
+        normalized[p.id] = p.value;
+      }
+
+      return normalized;
+    },
+    /**
+     * @param switchVariableObjectId {
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['ProjectCommon'] |
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['SelfObject'] |
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['ParentObject']
+     * }
+     * @param instanceId {number}
+     * @returns {
+     *   import("pgmmv-types/lib/agtk/object-instances/object-instance").AgtkObjectInstance |
+     *   import("pgmmv-types/lib/agtk/constants/switch-variable-objects").AgtkSwitchVariableObjects['ProjectCommon'] |
+     *   import("pgmmv-types/lib/agtk/constants/action-commands").AgtkActionCommands['UnsetObject']
+     * }
+     */
+    resolveSwitchVariableObject = function (switchVariableObjectId, instanceId) {
+      var instance = Agtk.objectInstances.get(instanceId),
+        pId;
+
+      switch (switchVariableObjectId) {
+        case Agtk.constants.switchVariableObjects.ProjectCommon:
+          return switchVariableObjectId;
+        case Agtk.constants.switchVariableObjects.SelfObject:
+          return instance;
+        case Agtk.constants.switchVariableObjects.ParentObject:
+          pId = instance.variables.get(Agtk.constants.objects.variables.ParentObjectInstanceIDId).getValue();
+
+          if (pId !== Agtk.constants.actionCommands.UnsetObject) {
+            return Agtk.objectInstances.get(pId);
+          }
+
+          break;
+        default:
+          break;
+      }
+
+      return Agtk.constants.actionCommands.UnsetObject;
     };
 
   return plugin;
